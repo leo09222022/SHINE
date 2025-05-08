03f90c41274ea0bef3e3562eb5c80f3a1d3fc176
jiewan02 <jiewan2002@gmail.com>
2025-05-08 10:20:06 +0900

    feature: 데이터 위치 변경, index.html실행시 데이터 모두 생성되게 변경

--- shine_semi/src/main/java/com/emerlet/controller/MapServlet.java
+++ shine_semi/src/main/java/com/emerlet/controller/MapServlet.java
@@ -32,19 +32,33 @@ public class MapServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // ToiletDAO 객체 생성
         ToiletDAO toiletDAO = new ToiletDAO();
-        
-        // findAll() 메서드를 통해 모든 화장실 데이터 가져오기
+
+        // CSV 경로 설정 (웹서버 루트 기준)
+        String csvPath = getServletContext().getRealPath("공중화장실.csv");
+        toiletDAO.setupDB(csvPath);
+
         ArrayList<ToiletVO> toilets = toiletDAO.findAll();
-        
-        // 가져온 데이터를 request에 속성으로 저장
         request.setAttribute("toilets", toilets);
 
-        // map.jsp로 포워딩
         RequestDispatcher dispatcher = request.getRequestDispatcher("/map.jsp");
         dispatcher.forward(request, response);
     }
+
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        // ToiletDAO 객체 생성
+//        ToiletDAO toiletDAO = new ToiletDAO();
+//        
+//        // findAll() 메서드를 통해 모든 화장실 데이터 가져오기
+//        ArrayList<ToiletVO> toilets = toiletDAO.findAll();
+//        
+//        // 가져온 데이터를 request에 속성으로 저장
+//        request.setAttribute("toilets", toilets);
+//
+//        // map.jsp로 포워딩
+//        RequestDispatcher dispatcher = request.getRequestDispatcher("/map.jsp");
+//        dispatcher.forward(request, response);
+//    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
